aea67a636b9ee5bc6258a2b3b5fe5b08a5ce77f4
Prince kumar mishra <kmishraprince@gmail.com>
2024-09-27 01:37:14 +0530

    First rust Program

---
+++ main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let ans = is_even(11);
+    println!("{}", ans);
+}
+
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    } else {
+        return false;
+    }
+}
